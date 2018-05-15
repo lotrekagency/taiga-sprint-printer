93692f931605e3526901d795730f84dbae1718af
Andrea Stagi <stagi.andrea@gmail.com>
2018-05-15 11:17:26 +0200

    Better tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,4 +4,4 @@ from taiga_sprint_printer.utils import get_current_dir
 
 
 def test_current_directory():
-    assert 'print_taiga_sprint/taiga_sprint_printer' in get_current_dir()
+    assert get_current_dir().endswith('/taiga_sprint_printer')
